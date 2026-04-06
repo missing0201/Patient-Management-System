07c92ed24216431f62ee346af307995cf9733836
missing0201 <tsunheiyan@yahoo.com>
2026-04-06 21:30:58 +0100

    First commit

---
+++ patient-service/src/main/java/com/pm/patientservice/controller/patientController.java
@@ -0,0 +1,46 @@
+package com.pm.patientservice.controller;
+
+import com.pm.patientservice.dto.PatientRequestDTO;
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.dto.validators.CreatePatientValidationGroup;
+import com.pm.patientservice.service.PatientService;
+import jakarta.validation.Valid;
+import jakarta.validation.groups.Default;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/patients")
+public class patientController {
+    private final PatientService patientService;
+
+    public patientController(PatientService patientservice) {
+        this.patientService = patientservice;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PatientResponseDTO>> GetPatients(){
+        List<PatientResponseDTO> patients = patientService.getPatients();
+        return ResponseEntity.ok().body(patients);
+    }
+
+    @PostMapping
+    public ResponseEntity<PatientResponseDTO> createPatient(@Validated({Default.class, CreatePatientValidationGroup.class}) @RequestBody PatientRequestDTO patientRequestDTO){
+        PatientResponseDTO patientResponseDTO= patientService.createPatient(patientRequestDTO);
+
+        return ResponseEntity.ok().body(patientResponseDTO);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PatientResponseDTO> updatePatient(@PathVariable UUID id,@Validated({Default.class}) @RequestBody PatientRequestDTO patientRequestDTO){
+        PatientResponseDTO patientResponseDTO= patientService.updatePatient(id,patientRequestDTO);
+
+        return ResponseEntity.ok().body(patientResponseDTO);
+    }
+
+}
---
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -0,0 +1,58 @@
+package com.pm.patientservice.service;
+
+import com.pm.patientservice.dto.PatientRequestDTO;
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.exception.EmailAlreadyExistException;
+import com.pm.patientservice.exception.PatientNotFoundException;
+import com.pm.patientservice.mapper.PatientMapper;
+import com.pm.patientservice.model.Patient;
+import com.pm.patientservice.repository.PatientRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class PatientService {
+    private PatientRepository patientRepository;
+
+    public PatientService(PatientRepository patientRepository){
+        this.patientRepository=patientRepository;
+    }
+
+    public List<PatientResponseDTO>getPatients(){
+        List<Patient> patients=patientRepository.findAll();
+
+        List<PatientResponseDTO> patientResponseDTOs=
+                patients.stream().map(PatientMapper::toDTO).toList();
+
+        return patientResponseDTOs;
+    }
+
+    public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO){
+        if(patientRepository.existsByEmail(patientRequestDTO.getEmail())){
+            throw new EmailAlreadyExistException("A patient with this email: "+patientRequestDTO.getEmail()+ " already exists.");
+        }
+        Patient newPatient= patientRepository.save(
+                PatientMapper.toModel(patientRequestDTO));
+
+                return PatientMapper.toDTO(newPatient);
+    }
+
+    public  PatientResponseDTO updatePatient(UUID id, PatientRequestDTO patientRequestDTO){
+        Patient patient = patientRepository.findById(id).orElseThrow(() -> new PatientNotFoundException("Patient not found with ID:"+id));
+        if(patientRepository.existsByEmail(patientRequestDTO.getEmail())){
+            throw new EmailAlreadyExistException("A patient with this email: "+patientRequestDTO.getEmail()+ " already exists.");
+        }
+
+        patient.setName(patientRequestDTO.getName());
+        patient.setAddress(patientRequestDTO.getAddress());
+        patient.setEmail(patientRequestDTO.getEmail());
+        patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+
+        Patient updatedPatient= patientRepository.save(patient);
+
+        return PatientMapper.toDTO(updatedPatient);
+    }
+}
