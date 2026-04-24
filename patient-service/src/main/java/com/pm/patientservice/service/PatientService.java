b09d30b78091ae09d02149924b81c55f942a7c20
missing0201 <tsunheiyan@yahoo.com>
2026-04-24 18:44:46 +0100

    Update on kafka compatibility

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -4,6 +4,8 @@ import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.exception.EmailAlreadyExistException;
 import com.pm.patientservice.exception.PatientNotFoundException;
+import com.pm.patientservice.grpc.BillingServiceGrpcClient;
+import com.pm.patientservice.kafka.KafkaProducer;
 import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
@@ -16,10 +18,14 @@ import java.util.UUID;
 
 @Service
 public class PatientService {
+    private final BillingServiceGrpcClient billingServiceGrpcClient;
+    private final KafkaProducer kafkaProducer;
     private PatientRepository patientRepository;
 
-    public PatientService(PatientRepository patientRepository){
+    public PatientService(PatientRepository patientRepository, BillingServiceGrpcClient billingServiceGrpcClient, KafkaProducer kafkaProducer){
         this.patientRepository=patientRepository;
+        this.billingServiceGrpcClient = billingServiceGrpcClient;
+        this.kafkaProducer = kafkaProducer;
     }
 
     public List<PatientResponseDTO>getPatients(){
@@ -38,7 +44,12 @@ public class PatientService {
         Patient newPatient= patientRepository.save(
                 PatientMapper.toModel(patientRequestDTO));
 
-                return PatientMapper.toDTO(newPatient);
+        billingServiceGrpcClient.createBillingAccount(newPatient.getId().toString()
+                ,newPatient.getName(), newPatient.getEmail());
+
+        kafkaProducer.sendEvent(newPatient);
+
+        return PatientMapper.toDTO(newPatient);
     }
 
     public  PatientResponseDTO updatePatient(UUID id, PatientRequestDTO patientRequestDTO){
